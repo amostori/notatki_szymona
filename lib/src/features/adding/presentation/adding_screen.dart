10a098488f0ed4363cee533e457ba425913f44bd
Marcin Andrzejczak <amostori@op.pl>
2024-04-20 09:45:52 +0200

    only body

--- lib/src/features/adding/presentation/adding_screen.dart
+++ lib/src/features/adding/presentation/adding_screen.dart
@@ -7,8 +7,12 @@ import 'package:second_memory/src/utils/routing/routing.dart';
 
 class AddingScreen extends ConsumerWidget {
   AddingScreen({super.key});
-  final titleController = TextEditingController();
+  // final titleController = TextEditingController();
   final bodyController = TextEditingController();
+  void _showInfoAboutDot(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Notatka musi zawierać kropkę (.)')));
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -20,35 +24,42 @@ class AddingScreen extends ConsumerWidget {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
+            // const SizedBox(
+            //   height: 30.0,
+            // ),
+            // TextField(
+            //   controller: titleController,
+            //   decoration: const InputDecoration(
+            //       labelText: 'Tytuł', border: OutlineInputBorder()),
+            // ),
             const SizedBox(
-              height: 30.0,
-            ),
-            TextField(
-              controller: titleController,
-              decoration: const InputDecoration(
-                  labelText: 'Tytuł', border: OutlineInputBorder()),
-            ),
-            const SizedBox(
-              height: 30.0,
+              height: 15.0,
             ),
             TextField(
               controller: bodyController,
-              maxLines: null,
+              maxLines: 4,
               decoration: const InputDecoration(
-                  labelText: 'Treść', border: OutlineInputBorder()),
+                  hintText:
+                      'Pierwsze zdanie zakończ kropką. Będzie tytułem notatki.',
+                  border: OutlineInputBorder()),
             ),
             const SizedBox(
-              height: 30.0,
+              height: 15.0,
             ),
             ElevatedButton(
               onPressed: () {
-                if (titleController.text.isNotEmpty &&
-                    bodyController.text.isNotEmpty) {
-                  final title = titleController.text;
+                if (bodyController.text.isNotEmpty &&
+                    bodyController.text.contains('.')) {
+                  final title = bodyController.text.split('.')[0];
                   final body = bodyController.text;
                   final note = NoteModel(title: title, body: body);
                   ref.read(noteProvider.notifier).addNote(note);
                   context.goNamed(AppRoute.home.name);
+                } else {
+                  if (!bodyController.text.contains('.')) {
+                    _showInfoAboutDot(context);
+                    return;
+                  }
                 }
               },
               child: const Text('Zapisz'),
